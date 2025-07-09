76ad839555bce7f9bd32beee09c1e04b1234660c
Amelia Magee <amelia.magee@outlook.com>
2025-07-09 05:27:50 +0100

    Fix docker implementation on systems without custom security profiles

--- backend/container-entrypoint.sh
+++ backend/container-entrypoint.sh
@@ -4,7 +4,7 @@ set -e;
 # Whilst this script is not doing anything the dockerfile can't,
 # it is useful to run processes after docker-compose has mounted volumes
 # otherwise our installed files at build time would be overwritten with any mounted volumes.
-
+mkdir -p vendor;
 composer install && composer dump-autoload;
 
 exec php-fpm;
